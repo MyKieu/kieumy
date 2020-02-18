cb668b0464465f9cd92c50b5d793f5c1cdaf6844
Your Name <you@example.com>
2020-02-18 15:52:48 +0700

    submit ex 1

--- src/main/java/basic/Exercise1.java
+++ src/main/java/basic/Exercise1.java
@@ -1,4 +1,17 @@
 package basic;
 
 public class Exercise1 {
+    public int timUcln(int a, int b) {
+        if (a == 0 || b == 0)
+            return 0;
+
+        if (a % b == 0) {
+            return b;
+        }
+        return timUcln(b, a % b);
+    }
+
+    public int timBcnn(int a, int b) {
+        return (a * b) / timUcln(a, b);
+    }
 }
--- src/test/java/basictest/Exercise1Test.java
+++ src/test/java/basictest/Exercise1Test.java
@@ -1,4 +1,33 @@
 package basictest;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import basic.Exercise1;
+
 public class Exercise1Test {
+    @Test
+
+    public void test_timUcln(){
+        assertEquals(new Exercise1().timUcln(12,168),12);
+        assertEquals(new Exercise1().timUcln(0,20),0);
+        assertEquals(new Exercise1().timUcln(20,0),0);
+        assertEquals(new Exercise1().timUcln(168,12),12);
+        assertEquals(new Exercise1().timUcln(12,12),12);
+        assertEquals(new Exercise1().timUcln(169,12),1);
+        assertEquals(new Exercise1().timUcln(30,20),10);
+        assertEquals(new Exercise1().timUcln(36,28),4);
+        assertEquals(new Exercise1().timUcln(5, 15),5);
+    }
+
+    @Test
+
+    public void test_timBcnn()
+    {
+        assertEquals(new Exercise1().timBcnn(3,4),12);
+        assertEquals(new Exercise1().timBcnn(6,15),30);
+        assertEquals(new Exercise1().timBcnn(8,4),8);
+    }
+
 }

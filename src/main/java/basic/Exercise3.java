5b2f1a121f452dd455acf2788db6d6501352c2fb
Your Name <you@example.com>
2020-02-26 11:15:39 +0700

    fixbt3

--- src/main/java/basic/Exercise3.java
+++ src/main/java/basic/Exercise3.java
@@ -2,19 +2,22 @@ package basic;
 
 public class Exercise3 {
 
-    public String phanTichSoNguyen(int n){
+    public String phanTichSoNguyen(int n) {
+
         String kq = "";
-        if(n < 2)
+        if (n < 2)
             return kq + n;
-        for(int i = 2; i <= n; i++){
-            while (n % i == 0){
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
                 n /= i;
-                if(n==1)
-                    kq += i + "*";
+                if (n == 1)
+                    kq += i;
                 else
-                    n += i;
+                    kq += i + "*";
+
             }
         }
+
         return kq;
     }
 }
--- src/test/java/basictest/Exercise3Test.java
+++ src/test/java/basictest/Exercise3Test.java
@@ -4,18 +4,18 @@ import basic.Exercise3;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertEquals;
 
 public class Exercise3Test {
     @Test
     public void test_check(){
 
 
-        assertNotEquals(new Exercise3().phanTichSoNguyen(1),1);
-        assertNotEquals(new Exercise3().phanTichSoNguyen(2),2);
-        assertNotEquals(new Exercise3().phanTichSoNguyen(28),2*2*7);
-        assertNotEquals(new Exercise3().phanTichSoNguyen(12),2*2*3);
-        assertNotEquals(new Exercise3().phanTichSoNguyen(600),2 * 2 * 2 * 3 * 5 * 5);
+        assertEquals(new Exercise3().phanTichSoNguyen(1),"1");
+        assertEquals(new Exercise3().phanTichSoNguyen(28),"2*2*7");
+        assertEquals(new Exercise3().phanTichSoNguyen(90),"2*3*3*5");
+        assertEquals(new Exercise3().phanTichSoNguyen(66),"2*3*11");
+
 
     }
 
